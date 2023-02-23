9751020f25e2e4d0c8fa15adb79251dddc291ebb
unknown <you@example.com>
2023-02-23 18:40:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Col, Container, Row } from "react-bootstrap";
+import React , { useState } from 'react'
+import FormInput from './Components/FormInput'
+import { question } from './data'
+import QAList from './Components/QAList'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
+
+    const [data , setData] =  useState(question)
+
+    const addItem =() => {
+      localStorage.setItem("items",JSON.stringify([...question]));
+      setData([...question])
+      notify("تم الاضافة بنجاح" , "Success")
+    }
+
+    const deleteAllItems =() => {
+      localStorage.removeItem("items");
+      question.splice(0 , question.length);
+      //اي بمعنا امسح المصفوفة الاصلية من الاول للاخر
+      setData([])
+      notify("تم حذف الكل بنجاح" , "Success")
+    }
+
+
+    const deleteOneItem = (items) => {
+      localStorage.setItem("items",JSON.stringify([...items]));
+      setData([...items])
+      if(items.length <= 0) {
+        deleteAllItems();
+        notify("تم حذف السؤال بنجاح" , "Success")
+      }
+    }
+
+
+    const notify = (message , type) =>{
+      if(type==="Error")
+      toast.error(message);
+      else if (type==="Success")
+      toast.success(message);
+    }; 
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font text-center color-body">
+      <Container className="p-5">
+        <Row className="justify-content-center">
+          <Col className="sm-4">
+            <div className="fs-3 text-center py-2">اسالة واجوبة شائعة</div>
+          </Col>
+          <Col className="sm-8">
+            <FormInput onAdd={addItem} notify={notify}/>
+            <QAList data={data} deleteOneItem={deleteOneItem} />
+
+            {
+            localStorage.getItem("items") != null? (<button onClick={deleteAllItems} className="btn-color w-100 my-3">مسح الكل</button>): null 
+            }
+            
+          </Col>
+        </Row>
+        <ToastContainer/>
+      </Container>
     </div>
   );
 }
